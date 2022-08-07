ee5cfbe37d7c8b442ac4bbe3c1fd6359a08297b5
Choisooyoung98 <ekfms15815@gmail.com>
2022-08-07 19:08:58 +0900

    정적 사이트 생성 hello-static.html, HelloController에 hello-templete.html 컨트롤러 입력 html도 같이 생성

--- src/main/java/hello/hellospring/comtroller/HelloController.java
+++ src/main/java/hello/hellospring/comtroller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.comtroller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -10,6 +11,13 @@ public class HelloController {
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data","hello!!");
+        model.addAttribute("data2","hello!!!!!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc") // url에서 hello-mvc 라는 값을 입력 받으면
+    public String helloMvc(@RequestParam("name") String name, Model model) { //외부에서 name이라는 파라미터를 받는다.
+        model.addAttribute("name", name); // 앞에 Key, 뒤에 값
+        return "hello-templete"; // hello-templete.html 에 리턴한다.
     }
 }
