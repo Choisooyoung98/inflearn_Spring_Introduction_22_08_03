b2a39dfa346ce951d6b0049fee25ac90193589fe
Choisooyoung98 <ekfms15815@gmail.com>
2022-08-07 19:14:13 +0900

    HelloController에서 helloMvc 메서드에 RequestParam의 defaultValue 추가

--- src/main/java/hello/hellospring/comtroller/HelloController.java
+++ src/main/java/hello/hellospring/comtroller/HelloController.java
@@ -16,7 +16,7 @@ public class HelloController {
     }
 
     @GetMapping("hello-mvc") // url에서 hello-mvc 라는 값을 입력 받으면
-    public String helloMvc(@RequestParam("name") String name, Model model) { //외부에서 name이라는 파라미터를 받는다.
+    public String helloMvc(@RequestParam(value = "name", defaultValue = "default값") String name, Model model) { //외부에서 name이라는 파라미터를 받는다, 값이 없으면 defaultValue 리턴
         model.addAttribute("name", name); // 앞에 Key, 뒤에 값
         return "hello-templete"; // hello-templete.html 에 리턴한다.
     }
