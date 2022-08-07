7901cad4b638fafc9658fe07e7f38fc902878469
Choisooyoung98 <ekfms15815@gmail.com>
2022-08-08 01:25:26 +0900

    MemberService에 Test클래스 생성 후 각각의 메서드들 테스트, Test클래스에 MemberService 클래스에서 레포지터리를 가져온다 -> DI

---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,76 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    // test가 각각 실행하기 전에 실행, memberRepository를 외부에서 가져온다. - DI dependency Injection
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void 회원가입() {
+        //test는 뭔가가 주어졌을 때(given) 이걸 실행했을 때(when) 이게 나와야돼(then) 라고 생각하면서 짜면 된다.
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        //assertThrows(예외, 로직) - 로직을 실행했을때 이 예외가 터진다.
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+        /*
+        try catch 구문 이용하기
+        try {
+            memberService.join(member2);
+            fail();
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        }
+        */
+        //then
+    }
+
+    @Test
+    void findOne() {
+    }
+}
