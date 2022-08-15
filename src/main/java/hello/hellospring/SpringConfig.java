c74fabcf290c372a8339b8347f1cb53e9c8f2aee
Choisooyoung98 <ekfms15815@gmail.com>
2022-08-16 00:02:22 +0900

    h2까지 연결

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,32 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+// 스프링빈에 AutoWise 하지않고 직접 연결시켜주는 것
+@Configuration
+public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new JdbcMemberRepository(dataSource);
+    }
+}
