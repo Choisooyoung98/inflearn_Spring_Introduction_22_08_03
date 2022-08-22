26b3b0f4e377ff8e5c1627a6e6f68ddb3e10cafd
Choisooyoung98 <ekfms15815@gmail.com>
2022-08-22 17:19:38 +0900

    jpa 연결 후 member에 jpa 연결

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,15 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity // jpa는 orm 객체의 오브젝트와 relation database 테이블을 mapping 한다. Jpa가 관리하는 Entity이다.
 public class Member {
 
+    @Id // primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 디비가 알아서 생성해주는 primarykey? 라고 보면된다.
     private Long id;
+
+    @Column(name = "username") // DB 컬럼 명이 "username" 이면 이렇게 명시해준다.
     private String name;
 
     public Long getId() {
